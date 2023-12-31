7e21cf7e7123b7ecbac0fec15fa1cfe2551b5314
yasithranusha <yasithranusha24@gmail.com>
2023-12-31 19:49:11 +0530

    fix : News letter subscribe mail alert

--- src/main/java/com/dune/greensupermarketbackend/news_letter/service/impl/NewsLetterImpl.java
+++ src/main/java/com/dune/greensupermarketbackend/news_letter/service/impl/NewsLetterImpl.java
@@ -1,6 +1,8 @@
 package com.dune.greensupermarketbackend.news_letter.service.impl;
 
 import com.dune.greensupermarketbackend.exception.APIException;
+import com.dune.greensupermarketbackend.mail.dto.EmailData;
+import com.dune.greensupermarketbackend.mail.service.MailService;
 import com.dune.greensupermarketbackend.news_letter.NewsLetterEntity;
 import com.dune.greensupermarketbackend.news_letter.NewsLetterRepository;
 import com.dune.greensupermarketbackend.news_letter.dto.NewsLetterDto;
@@ -18,6 +20,7 @@ import java.util.stream.Collectors;
 public class NewsLetterImpl implements NewsLetterService {
     private final NewsLetterRepository newsLetterRepository;
     private final ModelMapper modelMapper;
+    private final MailService mailService;
 
     public NewsLetterImpl(NewsLetterRepository newsLetterRepository) {
         this.newsLetterRepository = newsLetterRepository;
@@ -41,6 +44,36 @@ public class NewsLetterImpl implements NewsLetterService {
 
         NewsLetterEntity newsLetterEntity = modelMapper.map(newsLetterDto, NewsLetterEntity.class);
         newsLetterRepository.save(newsLetterEntity);
+
+        String htmlBody =
+                "<!DOCTYPE html>" +
+                        "<html>" +
+                        "<head>" +
+                        "<link href='https://fonts.googleapis.com/css2?family=Poppins&display=swap' rel='stylesheet'>" +
+                        "<style>" +
+                        "body { font-family: 'Poppins', sans-serif; }" +
+                        "h3 { color: #191919; }" +
+                        "p { color: rgb(71, 71, 71); }" +
+                        "</style>" +
+                        "</head>" +
+                        "<body>" +
+                        "<h3>Thank you for subscribing to our newsletter!</h3>" +
+                        "<p>Hi,</p>" +
+                        "<p>We're excited to have you on board. You can look forward to updates, exclusive offers, and much more delivered straight to your inbox.</p>" +
+                        "<p>Thank you,</p>" +
+                        "<p><span style=\"color: #00b207; font-weight: 500\">GREEN </span><span style=\"color: black; font-weight: 500\">SUPERMARKET</span></p>" +
+                        "</body>" +
+                        "</html>";
+
+        EmailData emailData = new EmailData(
+                newsLetterDto.getEmail(),
+                newsLetterDto.getEmail(),
+                "Welcome to Green Supermarket!",
+                htmlBody
+        );
+
+        mailService.sendMail(emailData);
+
         return new NewsLetterResponseDto("Email added successfully", newsLetterDto.getEmail());
     }
 
